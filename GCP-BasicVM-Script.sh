1e5ad3d10e2cdd67e0c1bb1b593127d149519a34
TomLLew <thomas.llewelyn@academytrainee.com>
2019-12-19 14:20:45 +0000

    edited scripts

--- GCP-BasicVM-Script.sh
+++ GCP-BasicVM-Script.sh
@@ -1,5 +1,12 @@
-#! bin bash
+#! /bin/bash
 echo "Pick a VM name: "
 read name
 gcloud compute instances create ${name}
-gcloud compute ssh ${name} 
+sleep 5
+gcloud compute ssh ${name} << EOF
+sudo apt-get -y update 
+sudo apt-get install git -y
+sudo apt-get install python3.7 -y
+sudo apt-get install python3-pip -y
+EOF
+gcloud compute ssh ${name}
